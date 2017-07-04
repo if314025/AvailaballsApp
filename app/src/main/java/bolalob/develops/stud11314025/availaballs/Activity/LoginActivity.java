6ba02429bb919a1c85c41149651ffbcb057a1d83
Okta <okta.p.nainggolan@gmail.com>
2017-07-04 14:34:47 +0700

    add MVP to login

--- app/src/main/java/bolalob/develops/stud11314025/availaballs/Activity/LoginActivity.java
+++ app/src/main/java/bolalob/develops/stud11314025/availaballs/Activity/LoginActivity.java
@@ -12,13 +12,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import bolalob.develops.stud11314025.availaballs.LoginMVP.LoginPresenter;
+import bolalob.develops.stud11314025.availaballs.LoginMVP.LoginPresenterImp;
+import bolalob.develops.stud11314025.availaballs.LoginMVP.LoginView;
 import bolalob.develops.stud11314025.availaballs.R;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 //import android.support.design.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView {
 
     @Bind(R.id.eTEmail)
     EditText etEmail;
@@ -26,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText etPassword;
     @Bind(R.id.btnLogin)
     Button btnLgn;
+    private LoginPresenter presenter;
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -33,43 +38,51 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        presenter = new LoginPresenterImp(this);
+
         btnLgn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String tempUsername = etEmail.getText().toString();
-                final String tempPassword = etPassword.getText().toString();
-                if(tempUsername.equals("test@gmail.com")&&tempPassword.equals("test")){
-                    Intent mainIntent =new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(mainIntent);
-                }
-                else if (tempPassword.isEmpty()||tempUsername.isEmpty()){
-                    Snackbar snackbar = Snackbar.make(view, "Email atau Password Kosong", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null);
-                    View sbView = snackbar.getView();
-                    sbView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSnackbar));
-
-                    TextView tv =(TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
-                        tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                    else
-                        tv.setGravity(Gravity.CENTER_HORIZONTAL);
-                    snackbar.show();
-                }
-                else {
-                    Snackbar snackbar = Snackbar
-                            .make(findViewById(R.id.activity_login), "Akun Tidak Terdaftar", Snackbar.LENGTH_SHORT)
-                            .setAction("Action", null);
-                    View sbView = snackbar.getView();
-                    sbView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSnackbar));
-
-                    TextView tv =(TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
-                        tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                    else
-                        tv.setGravity(Gravity.CENTER_HORIZONTAL);
-                    snackbar.show();
-                }
+                presenter.login(etEmail.getText().toString(), etPassword.getText().toString());
             }
         });
+    }
+
+    @Override
+    public void showValidationError() {
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.activity_login), "Email atau Password Kosong", Snackbar.LENGTH_LONG)
+                .setAction("Action", null);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSnackbar));
+
+        TextView tv = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+            tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        else
+            tv.setGravity(Gravity.CENTER_HORIZONTAL);
+        snackbar.show();
+    }
+
+    @Override
+    public void loginSucess() {
+        Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(mainIntent);
+    }
+
+    @Override
+    public void loginError() {
+        Snackbar snackbar = Snackbar
+                .make(findViewById(R.id.activity_login), "Akun Tidak Terdaftar", Snackbar.LENGTH_SHORT)
+                .setAction("Action", null);
+        View sbView = snackbar.getView();
+        sbView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSnackbar));
+
+        TextView tv = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+            tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        else
+            tv.setGravity(Gravity.CENTER_HORIZONTAL);
+        snackbar.show();
+
     }
 }
